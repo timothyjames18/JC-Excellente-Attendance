bf9db108740b168118506d39334d09435842b5f9
Timothy <tjblazo21@gmail.com>
2026-05-17 16:56:41 +0800

    a

--- assets/js/auth.js
+++
@@ -1,3 +0,0 @@
-export async function login(email,password){console.log(email,password)}
-export async function logout(){window.location='/index.html'}
-export function requireRole(){return true}
--- assets/js/firestore.js
+++
@@ -1,4 +0,0 @@
-export async function getStudent(qrCode){return null}
-export async function logAttendance(studentData,type){return true}
-export async function getAttendanceByDate(date){return []}
-export async function getStudents(){return []}
